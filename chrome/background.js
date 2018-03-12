47a988160a483c8edd51e0d6ab0105c73855db39
Yaqing Jiang <s1318268@ganymede.inf.ed.ac.uk>
2018-03-12 11:42:22 +0000

    m10 redirect
    using original indention
    
    redundant bracket

--- chrome/background.js
+++ chrome/background.js
@@ -8,5 +8,26 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
       details.requestHeaders.push(header)
       return {requestHeaders: details.requestHeaders}
     }, {urls: ['*://music.163.com/*']}, ['blocking', 'requestHeaders'])
-  }
-})
+  
+	var settings = {
+	    'domain': "m10.music.126.net",
+	    'ip': "ws.acgvideo.com",
+	    'enabled': true
+	}, requestFilter = {urls: ["<all_urls>"]},
+	    onBeforeRequestHandler = function(details) {
+		if (settings.enabled && (details.url.indexOf(settings.domain) > -1)) {
+		    return { redirectUrl: details.url.replace(settings.domain, settings.ip) };
+		}
+	    },
+	    onBeforeSendHeadersHandler = function(details) {
+		if (settings.enabled && (details.url.indexOf(settings.ip) > -1)) {
+		    details.requestHeaders.push({ name: "Host", value: settings.domain });
+		    return {requestHeaders: details.requestHeaders};
+		}
+	    };
+	chrome.webRequest.onBeforeRequest.addListener(onBeforeRequestHandler,   {urls: ['*://'+settings.domain+'/*']}, ["blocking"]);
+	chrome.webRequest.onBeforeSendHeaders.addListener(onBeforeSendHeadersHandler,{urls:['*://'+settings.ip+'/*']}, ["blocking", "requestHeaders"]);
+    }
+});
+
+
